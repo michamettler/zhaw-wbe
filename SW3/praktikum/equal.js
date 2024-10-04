f5b7c02406d563158dcf293815d0166c91b11ad0
mettlmi1 <mettlermicha@gmail.com>
2024-10-04 22:28:41 +0200

    finished lab03

---
+++ SW3/praktikum/equal.js
@@ -0,0 +1,12 @@
+const equal = function (a, b) {
+  if (typeof a !== "object" || typeof b !== "object") return a === b
+  if (Object.keys(a).length === 0 && Object.keys(b).length === 0) return true
+  if (Object.keys(a).length !== Object.keys(b).length) return false
+
+  for (let attr of Object.keys(a)) {
+    if (a[attr] !== b[attr]) return false
+  }
+  return true
+};
+
+module.exports = { equal } 
---
+++ SW3/praktikum/find-tag.js
@@ -0,0 +1,33 @@
+const findTag = (element) => {
+  let found = false
+  let word = ""
+  let currentlyBuilding = false
+
+  for (let i = 0; i < element.length; i++) {
+    if (found) break
+    let char = element[i]
+
+    if (currentlyBuilding) {
+      switch (char) {
+        case "<":
+          word = ""
+          currentlyBuilding = true
+          break
+        case ">":
+          found = true
+          break
+        case " ":
+          word = ""
+          currentlyBuilding = false
+          break
+        default:
+          word += element[i]
+      }
+    } else if (element[i] === "<") {
+      currentlyBuilding = true
+    }
+  }
+  return word !== "" ? word : undefined
+}
+
+module.exports = { findTag }
