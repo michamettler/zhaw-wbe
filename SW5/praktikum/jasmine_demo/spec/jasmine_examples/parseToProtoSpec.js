1c6fb17b3dd6d6c085ce40605478000c2f4a9580
mettlmi1 <mettlermicha@gmail.com>
2024-10-18 14:05:29 +0200

    finished lab05

---
+++ SW5/praktikum/jasmine_demo/spec/jasmine_examples/parseToProtoSpec.js
@@ -0,0 +1,16 @@
+const parseToProto = require('../../lib/jasmine_examples/parse-to-proto.js')
+
+describe('ParseToProto', function () {
+  let proto
+  let obj
+  
+  beforeEach(function () {
+    proto = { category: 'animal' }
+    obj = parseToProto.parseToProto('{"type":"cat","name":"Mimi","age":3}', proto)
+  })
+
+  it('should be able to parse', function () {
+    expect(obj.age).toEqual(3)
+    expect(obj.category).toEqual('animal')
+  })
+})
